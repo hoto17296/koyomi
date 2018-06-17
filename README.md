383ff90e30d222869cd7cfd2173fbd3029903c44
hoto17296 <mail@hoto.me>
2018-06-17 17:04:26 +0900

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Koyomi
+A Python module that deal with "二十四節気 (24 seasonal turning points of Japanese calendar)".
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+
+setup(
+    name='koyomi',
+    version='1.0',
+    description='A Python module that deal with "二十四節気 (24 seasonal turning points of Japanese calendar)".',
+    author='hoto17296',
+    license='MIT',
+    url='https://github.com/hoto17296/koyomi',
+    py_modules=['koyomi'],
+    install_requires=['numpy'],
+)
